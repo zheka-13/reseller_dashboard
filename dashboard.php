c4d8c5641317abaaffab0a60bf8f2dfc7cdd18d6
zheka <poliv78@gmail.com>
2022-06-29 07:27:10 +0300

    table sample

--- dashboard.php
+++ dashboard.php
@@ -1,2 +1,46 @@
 <?php
-echo "dashboard";
+/*
+	FusionPBX
+	Version: MPL 1.1
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+	The Original Code is FusionPBX
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2008-2016
+	the Initial Developer. All Rights Reserved.
+	Contributor(s):
+	KonradSC <konrd@yahoo.com>
+*/
+
+include "root.php";
+require_once "resources/require.php";
+require_once "resources/check_auth.php";
+require_once "resources/paging.php";
+
+if (permission_exists('reseller_dashboard_view')) {
+    //access granted
+}
+else {
+    echo "access denied";
+    exit;
+}
+$language = new text;
+$text = $language->get();
+require_once "resources/header.php";
+$document['title'] = $text['title-reseller_dashboard'];
+
+echo "<h1>Table sample</h1>";
+
+echo "<table class='tr_hover' width='100%' border='0' cellpadding='0' cellspacing='0'>\n";
+echo "<tr>\n";
+echo th_order_by('test', 'test', 'test', 'asc');
+echo "</tr>\n";
+echo "</table>";
+require_once "resources/footer.php";
