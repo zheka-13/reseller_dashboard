7f6a18cbffcf9cdc27a79d8f45598aed303bc6bf
zheka <poliv78@gmail.com>
2022-06-30 08:57:27 +0300

    domains stat v2

--- resources/classes/DomainStatService.php
+++ resources/classes/DomainStatService.php
@@ -21,7 +21,7 @@ class DomainStatService
         left join (select domain_uuid as ext_domain, count(*) as ext_count from v_extensions group by domain_uuid) b on (b.ext_domain=domain_uuid)
         left join (select domain_uuid as dev_domain, count(*) as dev_count from v_devices group by domain_uuid) c on (c.dev_domain=domain_uuid)
         left join (select domain_uuid as dest_domain, count(*) as dest_count from v_destinations group by domain_uuid) d on (d.dest_domain=domain_uuid)
-        left join (select domain_uuid as cc_domain, count(*) as cc_count from v_call_center_queues group by domain_uuid) b on (b.cc_domain=domain_uuid)
+        left join (select domain_uuid as cc_domain, count(*) as cc_count from v_call_center_queues group by domain_uuid) e on (e.cc_domain=domain_uuid)
         left join (select domain_uuid as vmail_domain, count(*) as vmails_count from v_voicemail_messages group by domain_uuid) f on (f.vmail_domain=domain_uuid)";
 
        return $this->db->select($query);
